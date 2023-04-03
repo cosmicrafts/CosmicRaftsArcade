09eafb6c4931cf1b7d5f17058b0578ed74f6faf4
sakunix <ssakuragi@gmail.com>
2023-04-03 16:25:00 -0700

    fix pantalla won

--- Menu/won.gd
+++ Menu/won.gd
@@ -1,9 +1,6 @@
-extends VBoxContainer
+extends Control
 
 
-func _on_restart_pressed():
-	get_tree().change_scene_to_file("res://Levels/level_1.tscn")
 
-
-func _on_menu_pressed():
+func _on_continue_pressed():
 	get_tree().change_scene_to_file("res://Menu/play.tscn")
